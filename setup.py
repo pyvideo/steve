a3c606d748c5a75df2718b42deaeb29aa98ff6fd
Will Kahn-Greene <willg@bluesock.org>
2012-08-06 11:41:22 -0400

    Add Python version classifiers

--- setup.py
+++ setup.py
@@ -62,5 +62,7 @@ setup(
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         ],
     )
