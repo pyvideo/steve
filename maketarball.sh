3d6a0f5b1d9209044c1c04029b6ecc9b46b74475
Will Kahn-Greene <willkg@bluesock.org>
2014-03-17 21:40:45 -0400

    Copyright updates

--- maketarball.sh
+++ maketarball.sh
@@ -3,7 +3,7 @@
 #######################################################################
 # This file is part of steve.
 #
-# Copyright (C) 2012, 2013 Will Kahn-Greene
+# Copyright (C) 2012-2014 Will Kahn-Greene
 # Licensed under the Simplified BSD License. See LICENSE for full
 # license.
 #######################################################################
